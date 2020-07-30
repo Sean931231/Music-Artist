d243f0e45d65adc8e243d394295d4d92f7fb031b
Sean931231 <seanwonghh93@gmail.com>
2020-07-30 18:02:52 +0800

    update
    update new version

--- mobile-version/src/main.js
+++ mobile-version/src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import "mand-mobile/components/_style/global.styl";
 import "normalize.css";
 
 Vue.config.productionTip = false
 Vue.prototype.$api = axios
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
--- mobile-version/src/views/Discography.vue
+++ mobile-version/src/views/Discography.vue
@@ -19,39 +19,40 @@
         :is-loop="false"
         :has-dots="false"
         @before-change="onSwiperChange"
+        class="swiper-part"
       >
         <md-swiper-item  v-for="item in items" :key="item.key">
-          <div class="empty-content" v-if="albums == null">
-            <p> - Not Yet Ready - </p>
-          </div>
+          <transition name="slide">
+            <div class="empty-content" v-if="albums == null">
+              <p> - Not Yet Ready - </p>
+            </div>
 
-          <div class="album-list" v-else>
-            <!-- <p> {{ albums.artist_name }}</p> -->
-            <div class="album-info" v-for="info in albums.album" :key="info.key">
-              <!-- <div class="album-title">
-                <p> {{ info.title }} </p>
-              </div> -->
+            <div class="album-list" v-else>
+              <!-- <p> {{ albums.artist_name }}</p> -->
+              <div class="album-info" v-for="info in albums.album" :key="info.key">
+                <div class="album-image" @click="toggle">
+                  <img :src="info.album_image" v-if="info.album_image"/>
+                  <img src="../assets/image/common/empty.jpg" v-else/>
+                </div>
 
-              <div class="album-image">
-                <img :src="info.album_image" />
-              </div>
+                <div class="album-release">
+                  <p v-if="info.release_date"> {{ info.release_date }} </p>
+                  <p v-else> Date Unknown </p>
+                </div>
 
-              <div class="album-release">
-                <p> {{ info.release_date }} </p>
-              </div>
-
-              <div class="album-brief">
-                <ol>
-                  <li v-for="(list, index) in info.playlist" :key="index">
-                    <p> {{list.brief}} </p>
-                    <a :href="list.url" v-if="list.url">
-                      <img src="../assets/image/social/icon_sns_black.png" >
-                    </a>
-                  </li>
-                </ol>
+                <div class="album-brief" v-if="info.playlist">
+                  <ol>
+                    <li v-for="(list, index) in info.playlist" :key="index">
+                      <p> {{list.brief}} </p>
+                      <a :href="list.url" v-if="list.url">
+                        <img src="../assets/image/social/icon_sns_black.png" >
+                      </a>
+                    </li>
+                  </ol>
+                </div>
               </div>
             </div>
-          </div>
+          </transition>
         </md-swiper-item>
       </md-swiper>
 
@@ -77,7 +78,8 @@ export default {
       currentName: "Yonezu Kenshi",
       artistData: [],
       items: [],
-      albums: []
+      albums: [],
+      isShow: false
     }
   },
 
@@ -99,7 +101,6 @@ export default {
     },
 
     tablist(index, ename) {
-      // console.log("api:"+ename);
       this.$api
           .get("/json/album.json", {})
           .then(response => {
@@ -114,6 +115,10 @@ export default {
               }
             }
           })
+    },
+
+    toggle() {
+        this.isShow = !this.isShow
     },
 
     onTabChange(item, index) {
