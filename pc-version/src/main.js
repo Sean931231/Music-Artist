d784c6a697a80d09bf02edab44500a9cee950651
Sean931231 <seanwonghh93@gmail.com>
2020-08-05 12:09:10 +0800

    update
    pc version add theme

--- pc-version/src/main.js
+++ pc-version/src/main.js
@@ -4,8 +4,11 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import vuetify from './plugins/vuetify';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
