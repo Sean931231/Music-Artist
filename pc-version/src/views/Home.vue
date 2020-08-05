d78489db7e55246626f30aa0e686fcffb9739973
Sean931231 <seanwonghh93@gmail.com>
2020-08-05 09:41:43 +0800

    update

--- pc-version/src/views/Home.vue
+++ pc-version/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DarkTitle />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import DarkTitle from '@/components/Title.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    DarkTitle
   }
 }
 </script>
