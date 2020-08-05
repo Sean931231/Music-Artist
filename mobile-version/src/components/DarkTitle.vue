590993762a5d0631ab85652e9b4e041a21443fc3
Sean931231 <seanwonghh93@gmail.com>
2020-08-05 09:40:55 +0800

    update

--- mobile-version/src/components/DarkTitle.vue
+++ mobile-version/src/components/DarkTitle.vue
@@ -1,29 +1,29 @@
 <template>
-    <div class="title-svg">
-        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-20 -30 600 650" width="250" height="150">
-            <defs>
-                <text id="JapText" x="287.69" y="92.11" font-size="400" font-weight="400" font-style="normal" xml:space="preserve">
-                <tspan x="287.69" dy="0em" alignment-baseline="before-edge" dominant-baseline="text-before-edge" text-anchor="middle">
-                PAGE
-                </tspan>
-                </text>    
-            </defs>
-            <g>
-                <use xlink:href="#JapText" 
-                    opacity="1" 
-                    fill-opacity="1" 
-                    fill="#000" 
-                    stroke="none" 
-                    stroke-width="3" 
-                    stroke-opacity="1"
-                >
-                </use>
-            </g>
-        </svg>
+  <div class="title-svg">
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="-20 -30 600 650" width="250" height="150">
+      <defs>
+        <text id="JapText" x="287.69" y="92.11" font-size="400" font-weight="400" font-style="normal" xml:space="preserve">
+        <tspan x="287.69" dy="0em" alignment-baseline="before-edge" dominant-baseline="text-before-edge" text-anchor="middle">
+        PAGE
+        </tspan>
+        </text>
+      </defs>
+      <g>
+        <use xlink:href="#JapText"
+            opacity="1"
+            fill-opacity="1"
+            fill="#000"
+            stroke="none"
+            stroke-width="3"
+            stroke-opacity="1"
+        >
+        </use>
+      </g>
+    </svg>
 
-    </div>
+  </div>
 </template>
 
 <style lang="scss">
-    @import "../styles/darktitle.scss";
+  @import "../styles/darktitle.scss";
 </style>
